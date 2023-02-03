b0b613a65304640340a3b6a6f6b61e551ca6652c
nakasyou <nakasyou1103@gmail.com>
2023-02-03 18:50:39 +0900

    fix code

--- test/wasm-test/main.ts
+++ test/wasm-test/main.ts
@@ -1,3 +1,5 @@
-const wasmCode = await Deno.readFile("./target/wasm32-unknown-unknown/debug/wasm_deno.wasm");
+const wasmPath = new URL("../../rs-wasm/build/main.wasm", import.meta.url)
+const wasmCode = await Deno.readFile(wasmPath);
 const wasmModule = new WebAssembly.Module(wasmCode);
+console.log(wasmModule)
 const wasmInstance = new WebAssembly.Instance(wasmModule);
